2119dfb36b834a395986cfa7fc6c7eec7aa3c78f
Yan_Pustynnyy <necrosith@yandex.ru>
2019-06-06 16:08:07 +0300

    Init commit. Lesson 1 Done

---
+++ Lesson 1/homework.js
@@ -0,0 +1,39 @@
+'use strict';
+let employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+
+// Filter the array to drop null or zero-length names
+employers = employers.filter((name) => {
+    return (name.length > 0 && name.length != '');
+});
+
+// Change the array to lowercase all the elements
+employers = employers.map((name) => {
+    return name.toLowerCase().trim();
+});
+
+const sponsors = {
+    cash: [40000, 5000, 30400, 12000],
+    eu: ['SRL', 'PLO', 'J&K'],
+    rus: ['RusAuto', 'SBO']
+};
+
+const calcCash = (own = 0, cash) => {
+    const everyCash = [...cash];
+    return everyCash.reduce((acc, cash) => acc + cash, own);
+}
+
+const money = calcCash(null, sponsors.cash);
+
+// Null handler function in case that director takes null value which can pass through default parameter algorithm
+const directorNullHandler = arg => (...a) => arg(...a.map(v => (v === null ? 'Victor' : v)))
+
+const makeBusiness = (owner, director, cash, emp) => {
+    directorNullHandler(director = 'Victor');
+    const {eu, rus} = sponsors;
+    const sumSponsors = [...eu, ...rus, 'unexpected sponsor'];
+    console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}.And our employers: ${emp}.
+And we have a sponsors: 
+${[...sumSponsors]}
+Note. Be careful with ${eu[0]}. It's a huge risk.`);
+}
+makeBusiness('Sam', null, money, employers);
