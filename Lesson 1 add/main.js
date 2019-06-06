728ba3e5fde565a20b8dc80d6c6866a9161f02c8
Yan_Pustynnyy <necrosith@yandex.ru>
2019-06-06 20:09:17 +0300

    Image displaying fixed. Now everything works. Image displaying was moved after all images are loaded

--- Lesson 1 add/main.js
+++ Lesson 1 add/main.js
@@ -1,22 +1,30 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
 
-let images = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-]
+    let images = [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+    ]
+    
+    const checkImage = path => {
+        return new Promise(resolve => {
+            const img = new Image();
+            img.onload = () => resolve(path);
+            img.onerror = () => console.error(`${path} not found`);
+    
+            img.src = path;
+        });
+    };
+    
+    let loadImage = (...images) => Promise.all(images.map(checkImage))
+        .then(() => {
+            for (let image of images) {
+                const img = `<img src="${image}"/>`;
+                document.body.innerHTML += img;
+            }
+        });
+    
+    loadImage(...images);
+});
 
-const checkImage = path => {
-    return new Promise(resolve => {
-        const img = new Image();
-        img.onload = () => resolve(path);
-        img.onerror = () => console.error(`${path} not found`);
-
-        // document.getElementsByTagName('body').appendChild(img);
-        img.src = path;
-    });
-};
-
-let loadImage = (...images) => Promise.all(images.map(checkImage));
-
-loadImage(...images);
